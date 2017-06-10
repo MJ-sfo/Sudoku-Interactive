6911d912db91fbbdf5ee5f30b015f2750f51d5c9
MJ-sfo <michaeljlaird@yahoo.com>
2017-06-10 15:36:06 -0700

    trying to create table on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,16 @@
 class WelcomeController < ApplicationController
+
+    attr_reader :arr_81
     def index
       string_starting_numbers = File.readlines("#{Rails.root}/config/sudoku_puzzles.txt")[0]
       @values = create_numbers(string_starting_numbers)
       render('index')
 
+    end
+    def arr_81
+      @arr_81 =[]
+      81.times {|i| @arr_81.push(i.to_i}
+      puts @arr_81
     end
 
 end
