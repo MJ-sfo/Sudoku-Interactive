4c3bac68bf0a183dc1fb556c094908b67d4671f2
MJ-sfo <michaeljlaird@yahoo.com>
2017-06-09 19:22:47 -0700

    rails takes text, breaks into array, print bad table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include ActionView::Helpers::SanitizeHelper
+
+  p "from ActionController !!!"
+
+
+
+  def create_numbers(string)
+    test_arr   = string.split("")
+    test_arr.map! do |index|
+      if index != "-"
+        index = index.to_i
+      else
+        index = "-"
+      end    # if index
+    end   # test_arr.map! do
+    test_arr
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
     def index
+      string_starting_numbers = File.readlines("#{Rails.root}/config/sudoku_puzzles.txt")[0]
+      @values = create_numbers(string_starting_numbers)
       render('index')
+
     end
+
 end
